e0106a8e9f77ba1eabfd6ef204c017871d318ef2
RobertoCetzalAlvarez <119071418+RobertoCetzalAlvarez@users.noreply.github.com>
2024-02-06 07:53:51 -0600

    dashboard

---
+++ resources/views/Dashboard.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cards -- tarjetas con efecto Hover</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <style>
+    body {  
+        font-family: 'Nunito', sans-serif;
+    }
+</style>
+</head>
+<body class="antialiased">
+
+    <header id="carrusel-contenido">
+        <div id="carrusel-caja">
+            <div class="carrusel-elemento">
+                <img class="imagenes" href="" src="img/utc5.jpeg" >
+            </div>
+
+            <div class="carrusel-elemento">   
+                <img class="imagenes" href="" src="img/utc1.png">
+            </div>
+
+            <div class="carrusel-elemento">   
+                <img class="imagenes" href="" src="img/utc7.jpeg">                        
+            </div>
+
+        </div>
+    </header>
+
+
+    <div class="container">
+        
+        <div class="card">
+            <img src="img/Supervicion_de_reparto_A_R.jpg">
+            <h4>Supervición de Reparto</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <a href="/login">Acceder</a>
+        </div>
+
+        <div class="card">
+            <img src="img/Supervicion_de_reparto_A_R.jpg">
+            <h4>Supervición de Cortes</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <a href="/login">Acceder</a>
+        </div>
+
+        <div class="card">
+            <img src="img/Supervicion_de_reparto_A_R.jpg">
+            <h4>Finanzas</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+            <a href="/login">Acceder</a>
+        </div>
+    </div>
+    
+</body>
+</html>
